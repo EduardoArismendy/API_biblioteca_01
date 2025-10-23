add52d7892b42a707751f4f67028aee6c78ec9f9
EduardoArismendy <p73306275@gmail.com>
2025-10-23 12:48:40 -0500

    add: models using entity

---
+++ API_Biblioteca_01/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Biblioteca_01.Models;
+
+public partial class Book
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public int? PublicationYear { get; set; }
+
+    public string? CoverImageUrl { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+    public virtual User User { get; set; } = null!;
+}
